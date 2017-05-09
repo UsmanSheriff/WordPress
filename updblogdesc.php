d7e6974e00015d0fd1f6cdc95f4adcec316c5fd1
Usman Sheriff <usman.sheriff@hpe.com>
2017-05-09 05:25:44 +0000

    Modify page subtitle

---
+++ updblogdesc.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "wordpressuser";
+$password = "bond007";
+$dbname = "wordpress";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "UPDATE wp_options SET option_value='Just !!! another WordPress' WHERE option_id=4";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
